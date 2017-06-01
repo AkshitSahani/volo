e6219f59b18eb4b783e6fe8fbc7a985c6b846623
drhie <rhie.david@gmail.com>
2017-06-01 17:26:14 -0400

    Fill out survey model, controller + nested questions in views

--- app/models/survey.rb
+++ app/models/survey.rb
@@ -2,4 +2,6 @@ class Survey < ApplicationRecord
   belongs_to :location
   has_many :questions
   has_many :responses, through: :questions
+
+  accepts_nested_attributes_for :questions, reject_if: :all_blank, allow_destroy: true
 end

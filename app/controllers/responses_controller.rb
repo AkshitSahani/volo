5b0b9bac909ab9c5a34cedbac865e6bff4eca58c
Akshit Sahani <akshit_sahani@hotmail.com>
2017-06-05 12:30:21 -0400

    minor changes to response controller

--- app/controllers/responses_controller.rb
+++ app/controllers/responses_controller.rb
@@ -3,9 +3,9 @@ class ResponsesController < ApplicationController
   def create
     params.each do |q_id|
       if User.find(session[:user_id]).account_type == "Resident"
-        Response.create(response: params["#{q_id}"], question_id: q_id, resident_id: session[:user_id]) if /^\d+$/.match(q_id)
-      else
-        Response.create(response: params["#{q_id}"], question_id: q_id, volunteer_id: session[:user_id]) if /^\d+$/.match(q_id)
+        Response.create(response: params["#{q_id}"], question_id: q_id, resident_id: (Resident.where(user_id: session[:user_id]).id) if /^\d+$/.match(q_id)
+      elsif User.find(session[:user_id]).account_type == "Volunteer"
+        Response.create(response: params["#{q_id}"], question_id: q_id, volunteer_id: (Volunteer.where(user_id: session[:user_id]).id) if /^\d+$/.match(q_id)
       end
     end
   end

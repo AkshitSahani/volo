008cee08e49891eebbf7996d447b94bdb9000572
Richard Strauss <rstrauss91@gmail.com>
2017-06-06 20:34:50 -0400

    add paperclip functionality

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,18 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  Paperclip.options[:command_path] = "/usr/local/bin/"
+
+  config.paperclip_defaults = {
+    storage: :s3,
+    s3_host_name: 's3.ca-central-1.amazonaws.com/',
+    s3_credentials: {
+      bucket: ENV.fetch('S3_BUCKET_NAME'),
+      access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
+      secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
+      s3_region: ENV.fetch('AWS_REGION'),
+    }
+  }
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
@@ -59,7 +71,7 @@ Rails.application.configure do
   # Suppress logger output for asset requests.
   config.assets.quiet = true
   config.assets.enabled = true
-  config.assets.paths << "#{Rails.root}/app/assets/fonts/roboto" 
+  config.assets.paths << "#{Rails.root}/app/assets/fonts/roboto"
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true

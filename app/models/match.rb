f905f7a6ff67f8ac601777b081ff5858bf0b556f
Akshit Sahani <akshit_sahani@hotmail.com>
2017-06-13 13:04:41 -0400

    Roboto font errors fixed

--- app/models/match.rb
+++ app/models/match.rb
@@ -21,12 +21,12 @@ class Match < ApplicationRecord
   def self.filters(surv, subject)
     filters = []
     surv.questions.each do |q|
-      if q.question_type == "Multiple Choice"
+      if q.question_type == "Multiple Choice Question"
         if !(subject.responses.where(question_id: q.id).empty?)
           subject_answers = subject.responses.where(question_id: q.id).first
         end
       filters << [q, subject_answers]
-      elsif q.question_type == "Drop-Down"
+    elsif q.question_type == "Drop-Down Question"
         subject_answers = subject.responses.where(question_id: q.id)[0] if !(subject.responses.where(question_id: q.id).empty?)
         filters << [q, subject_answers]
       end

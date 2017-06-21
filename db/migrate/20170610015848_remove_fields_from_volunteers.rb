d3066570b21cc7784b259c22a46639d2645e36a7
Akshit Sahani <akshit_sahani@hotmail.com>
2017-06-21 14:46:09 -0400

    fixed...

--- db/migrate/20170610015848_remove_fields_from_volunteers.rb
+++ db/migrate/20170610015848_remove_fields_from_volunteers.rb
@@ -3,6 +3,5 @@ class RemoveFieldsFromVolunteers < ActiveRecord::Migration[5.0]
     remove_column :volunteers, :birthdate, :integer
     remove_column :volunteers, :phone_number, :integer
     remove_column :residents, :birthdate, :integer
-    remove_column :residents, :birthdate, :integer
   end
 end

6aad6622ba737c94136ca2b1223af447ea87b0ae
Akshit Sahani <akshit_sahani@hotmail.com>
2017-06-21 14:54:40 -0400

    fixed...phew

--- db/migrate/20170610025504_remove_phonenumber_from_organizations.rb
+++ db/migrate/20170610025504_remove_phonenumber_from_organizations.rb
@@ -1,5 +1,5 @@
 class RemovePhonenumberFromOrganizations < ActiveRecord::Migration[5.0]
   def change
-    remove_column :organizations, :phonenumber, :string
+    remove_column :organizations, :phone_number, :string
   end
 end

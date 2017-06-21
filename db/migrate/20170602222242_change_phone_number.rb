d05bc025220682fef52e2ae3da8dfbeb005aa8d2
Akshit Sahani <akshit_sahani@hotmail.com>
2017-06-21 14:28:09 -0400

    minor fix

--- db/migrate/20170602222242_change_phone_number.rb
+++ db/migrate/20170602222242_change_phone_number.rb
@@ -1,7 +1,7 @@
 class ChangePhoneNumber < ActiveRecord::Migration[5.0]
   def change
     remove_column :organizations, :phone_number, :integer
-    remove_column :residents, :phone_number, :integer
+    # remove_column :residents, :phone_number, :integer
     remove_column :volunteers, :phone_number, :integer
     add_column :organizations, :phone_number, :string
     add_column :residents, :phone_number, :string
